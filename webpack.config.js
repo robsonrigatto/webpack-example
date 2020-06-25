d04793a95294ffa400e7772cc2c42e6b8f4199b8
robsonrigatto <rigatto.robson@gmail.com>
2020-06-24 22:52:34 -0300

    exemplo de webpack 4

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const modeDev = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+    mode: modeDev ? 'development' : 'production',
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename: 'estilo.css'
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCssAssetsWebpackPlugin({})
+        ]
+    },
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                //'style-loader', //Adiciona CSS utilizando a tag style,
+                'css-loader', //interpreta @import, url, etc
+                'sass-loader'
+            ]
+        },{
+            test: /\.(png|svg|jpg|gif)$/,
+            use: 'file-loader'
+        }]
+    }
+}
